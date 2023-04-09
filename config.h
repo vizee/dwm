39430c5ed2add72f47abdc19ec9bf3b91e9b9a2c
vizee <vizee@live.cn>
2023-04-09 13:47:49 +0800

    update config.h

--- config.h
+++ config.h
@@ -61,8 +61,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "exo-open", "--launch", "TerminalEmulator", NULL };
 static const char *webcmd[]  = { "exo-open", "--launch", "WebBrowser", NULL };
 static const char *fmcmd[]  = { "exo-open", "--launch", "FileManager", NULL };
@@ -71,7 +70,7 @@ static const char *volupcmd[] = { "pulseaudio-ctl", "up", NULL };
 static const char *voldowncmd[] = { "pulseaudio-ctl", "down", NULL };
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 
-static Key keys[] = {
+static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_grave,  spawn,          {.v = termcmd } },
@@ -123,7 +122,7 @@ static Key keys[] = {
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
